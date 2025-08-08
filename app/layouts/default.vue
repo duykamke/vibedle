ec2492091e109de631c6f7922b990468fc0e1ea6
Duy Nguyen <duykamke@gmail.com>
2025-08-08 09:16:50 +0000

    feat: add vercel insights and analytics

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,4 +1,6 @@
 <template>
+  <Analytics />
+  <SpeedInsights />
   <div class="min-h-screen flex flex-col">
     <AppHeader />
     <main class="flex-1">
@@ -8,3 +10,7 @@
 </template>
 
 
+<script setup lang="ts">
+import { Analytics } from '@vercel/analytics/nuxt'
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
+</script>
